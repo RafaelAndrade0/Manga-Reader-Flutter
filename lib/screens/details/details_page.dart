3e3070552dcbafce47cb5e32e62c96280cd43b1e
Rafael <rafael.andrade.dev0@gmail.com>
2020-07-23 15:31:39 -0300

    Alterando visual da TabBar

--- lib/screens/details/details_page.dart
+++ lib/screens/details/details_page.dart
@@ -14,26 +14,53 @@ class DetailsPage extends StatelessWidget {
           iconTheme: IconThemeData(
             color: Colors.black,
           ),
-          bottom: TabBar(
-            labelColor: Colors.black,
-            indicatorColor: Colors.black,
-            tabs: [
-              Tab(
-                text: 'Information',
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
               ),
-              Tab(
-                text: 'Chapters',
-              ),
-            ],
+              child: TabBar(
+                  unselectedLabelColor: Colors.black,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicator: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.blueAccent, Colors.blue[800]],
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  tabs: [
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "INFORMATION",
+                          style: TextStyle(
+                            fontFamily: "Anime Ace",
+                          ),
+                        ),
+                      ),
+                    ),
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "CHAPTERS",
+                          style: TextStyle(
+                            fontFamily: "Anime Ace",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
           ),
         ),
-        body: TabBarView(
-          children: [
-            // Icon(Icons.directions_transit),
-            MangaInformation(),
-            MangaChapters(),
-          ],
-        ),
+        body: TabBarView(children: [
+          MangaInformation(),
+          MangaChapters(),
+        ]),
       ),
     );
   }
